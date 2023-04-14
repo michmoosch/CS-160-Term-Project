0cf37ab47d92f49ba2ffaa74805ad06c78f20af5
yanli12 <78177686+yanli12@users.noreply.github.com>
2023-04-14 00:39:40 -0700

    Added driver and cartItem table

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,51 +1,67 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, ForeignKey
+
 
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    uid = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.VARCHAR(64), unique=True, nullable=False)
+    email = db.Column(db.VARCHAR(64), primary_key=True, nullable=False)
     pwd = db.Column(db.VARCHAR(128))
     fname = db.Column(db.VARCHAR(64))
     lname = db.Column(db.VARCHAR(64))
-
+    orderId = db.relationship("Order", uselist=False, backref="order_fk")
+    
 
 class Product(db.Model):
-    prodid = db.Column(db.INT, primary_key=True, nullable=False)
+    prodid = db.Column(db.Integer, primary_key=True)
     prodName = db.Column(db.VARCHAR(64), unique=True, nullable=False)
-    prodDescip = db.Column(db.TEXT)
-    prodUnitPrice = db.Column(db.DOUBLE)
-    prodUnitInStock = db.Column(db.INT)
-    prodUnitWeight = db.Column(db.DOUBLE)
+    prodDescip = db.Column(db.TEXT, nullable=False)
+    prodUnitPrice = db.Column(db.DOUBLE, nullable=False)
+    prodUnitInStock = db.Column(db.Integer, nullable=False)
+    prodUnitWeight = db.Column(db.DOUBLE, nullable=False)
+
+
+class Cart(db.Model):
+    cartid = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.Integer, ForeignKey(User.email), unique=True)
+    street = db.Column(db.TEXT)
+    city = db.Column(db.VARCHAR(16))
+    state = db.Column(db.VARCHAR(16))
+    zipcode = db.Column(db.Integer)
+
+class CartItem(db.Model):
+    __tablename__ = 'cartitem'
+    cartItemid = db.Column(db.INT, primary_key=True, nullable=False)
+    productId = db.Column(db.Integer, ForeignKey(Product.prodId))
+    cartId = db.Column(db.Integer, ForeignKey(Cart.cartId))
+    quantity = db.Column(db.INT)
+
+class userAddress(db.Model):
+    __tablename__ = 'address'
+    fname = db.Column(db.VARCHAR(64))
+    lname = db.Column(db.VARCHAR(64))
+    street = db.Column(db.TEXT)
+    city = db.Column(db.VARCHAR(64))
+    state = db.Column(db.VARCHAR(64))
+    zipcode = db.Column(db.Integer)
+
+class driver(db.Model):
+    __tablename__ = 'driver'
+    driverid = db.Column(db.Integer, primary_key=True, nullable=False)
+    fname = db.Column(db.VARCHAR(64))
+    lname = db.Column(db.VARCHAR(64))
+    status = db.Column(db.VARCHAR(20))
+    warehouse = db.Column(db.VARCHAR(20))
+    orderNum = db.Column(db.Integer)
+
+class Order(db.Model):
+    orderid = db.Column(db.INT, primary_key=True, nullable=False)
+    orderDate = db.Column(db.TEXT)
+    orderDeliveryMethod = db.Column(db.VARCHAR(64))
+    uid = db.Column(db.Integer, ForeignKey(User.email), unique=True)
 
 # class ShoppingSession(db.Model):
 #     cartID = db.Column(db.INT, primary_key=True, nullable=False)
 #     cartUserID = db.relationship('User', backref='cartsession', lazy=True)
 #     cartTotal = db.Column(db.DOUBLE)
-
-# class CartItem(db.Model):
-#     cartItemID = db.Column(db.INT, primarary_key=True, nullable=False)
-#     sessionID = db.relationship('ShoppingSession', backref='item', lazy=True)
-#     prodID = db.relationship('Product', backref='cart', lazy=True)
-#     quantity = db.Column(db.INT)
-=======
-#class ShoppingSession(db.Model):
-#    cartID = db.Column(db.INT, primary_key=True, nullable=False)
-#    cartUserID = db.relationship('User', backref='cartsession', lazy=True)
-#    cartTotal = db.Column(db.DOUBLE)
-
-class CartItem(db.Model):
-    cartItemID = db.Column(db.INT, primarary_key=True, nullable=False)
-    sessionID = db.relationship('ShoppingSession', backref='item', lazy=True)
-    prodID = db.relationship('Product', backref='cart', lazy=True)
-    quantity = db.Column(db.INT)
-#class CartItem(db.Model):
-#    cartItemID = db.Column(db.INT, primarary_key=True, nullable=False)
-#    sessionID = db.relationship('ShoppingSession', backref='item', lazy=True)
-#    prodID = db.relationship('Product', backref='cart', lazy=True)
-#    quantity = db.Column(db.INT)
-
-
-
