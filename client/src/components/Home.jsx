afad36aef7f1f8d550f7673c3533ee4c2a4a1d7e
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2023-05-03 14:23:26 -0700

    profil

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { parseCookie, CATEGORIES, imageImports,  } from "./util";
+import { parseCookie, CATEGORIES, imageImports } from "./util";
 import { getProducts } from "./api";
 import Signup from "./Signup";
 
-
 import { Link, useNavigate } from "react-router-dom";
-
-
 
 const Home = () => {
   const navigate = useNavigate();
@@ -22,14 +19,13 @@ const Home = () => {
     navigate("/login");
   }
 
-
   const getImage = (imageName) => {
     for (const image in images) {
       if (image.includes(imageName)) {
-        return images[image]
+        return images[image];
       }
     }
-  }
+  };
 
   const name = cookie.UserFirstName;
   const isAdmin = cookie.isAdmin;
@@ -42,13 +38,13 @@ const Home = () => {
       const obj = JSON.parse(data);
       setProducts((prev) => obj);
     }
-  //   async () => {
-  //     const info = await getProducts()
-  //   return info
-  // }
-  // const items = await ();
-  // setProducts((prev) => items);
-    getData()
+    //   async () => {
+    //     const info = await getProducts()
+    //   return info
+    // }
+    // const items = await ();
+    // setProducts((prev) => items);
+    getData();
   }, []);
 
   const logout = (e) => {
@@ -99,42 +95,40 @@ const Home = () => {
   const incItem = (item) => {
     if (item in cart) {
       setCart((prev) => {
-        const update = prev
+        const update = prev;
         update[item] += 1;
         console.log(update);
         return update;
       });
-    }
-    else {
+    } else {
       setCart((prev) => {
-        const update = prev
+        const update = prev;
         update[item] = 1;
         console.log(update);
         return update;
       });
     }
-    setItemsInCart((prev) => prev++)
-    console.log(itemsInCart)
-  }
+    setItemsInCart((prev) => prev++);
+    console.log(itemsInCart);
+  };
   const decItem = (item) => {
-    if (item in cart){
+    if (item in cart) {
       setCart((prev) => {
         const update = prev;
-        if (update[item] == 0){
-          return
+        if (update[item] == 0) {
+          return;
         }
         update[item] -= 1;
         console.log(update);
         return update;
       });
-  
     }
-  }
+  };
   const addToCart = (e) => {
-    for (const product in products){
-      if (products[product].ProductId == e.target.value){
-        console.log(products[product].ProductName)
-        incItem(products[product].ProductId)
+    for (const product in products) {
+      if (products[product].ProductId == e.target.value) {
+        console.log(products[product].ProductName);
+        incItem(products[product].ProductId);
       }
     }
     // console.log(e.target.value)
@@ -161,12 +155,16 @@ const Home = () => {
     // )
 
     // console.log(Object.keys(cart).length)
-  }
+  };
 
   const checkout = () => {
-    console.log(cart)
-    navigate("/checkout", {state: cart})
-  }
+    console.log(cart);
+    navigate("/checkout", { state: cart });
+  };
+
+  const profile = () => {
+    navigate("/profile");
+  };
 
   return (
     <div>
@@ -176,6 +174,9 @@ const Home = () => {
           Welcome, <p className="font-bold ml-1"> {name}</p>
         </div>
         {isAdmin && <Link>Admin</Link>}
+        <button className="btn" onClick={profile}>
+          Profile
+        </button>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
@@ -185,7 +186,10 @@ const Home = () => {
                     {itemsInCart}
                   </span>
                 )}
-                <button className="btn" onClick={checkout}>My Cart</button>
+
+                <button className="btn" onClick={checkout}>
+                  My Cart
+                </button>
               </div>
             </li>
 
@@ -195,19 +199,43 @@ const Home = () => {
           </ul>
         </div>
       </div>
-      {showCart &&  
-      <div className="fixed right-5 top-[12%] h-[500px] w-[200px] bg-info z-20">
-        {itemsInCart.length > 0 && itemsInCart.map((item) => {
-
-          return (
-          <div className="w-full flex flex-row justify-between bg-slate-200 text-xl">
-            <div>{item}</div>
-            <div className="w-1/4 flex flex-row justify-evenly"><button onClick={() => {decItem(item)}}> - </button>{cart[item]}<button onClick={() => {incItem(item)}}> + </button></div>
-            </div>
-            )
-        })}
-        <button className="btn btn-secondary absolute bottom-0" onClick={checkout}>Checkout</button>
-      </div>}
+      {showCart && (
+        <div className="fixed right-5 top-[12%] h-[500px] w-[200px] bg-info z-20">
+          {itemsInCart.length > 0 &&
+            itemsInCart.map((item) => {
+              return (
+                <div className="w-full flex flex-row justify-between bg-slate-200 text-xl">
+                  <div>{item}</div>
+                  <div className="w-1/4 flex flex-row justify-evenly">
+                    <button
+                      onClick={() => {
+                        decItem(item);
+                      }}
+                    >
+                      {" "}
+                      -{" "}
+                    </button>
+                    {cart[item]}
+                    <button
+                      onClick={() => {
+                        incItem(item);
+                      }}
+                    >
+                      {" "}
+                      +{" "}
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          <button
+            className="btn btn-secondary absolute bottom-0"
+            onClick={checkout}
+          >
+            Checkout
+          </button>
+        </div>
+      )}
       <div className="navbar bg-content w-screen flex flex-row items-center justify-center mr-5 pr-5">
         <div className="form-control w-[200px]">
           <label className="label"></label>
@@ -239,32 +267,31 @@ const Home = () => {
       <div className="container w-screen grid grid-cols-3 gap-4 justify-center w-screen pl-[15%]">
         {products.length > 0 &&
           products.map((product, index) => {
-        
-            const img = imageImports[product.ProductImage]
+            const img = imageImports[product.ProductImage];
             // console.log(product)
             return (
-              <div
-                key={index}
-                className="card bg-base-100 w-[200px] h-[200px]"
-              >
-                <figure><img src={img} alt={`${product.ProductName}`} /></figure>
+              <div key={index} className="card bg-base-100 w-[200px] h-[200px]">
+                <figure>
+                  <img src={img} alt={`${product.ProductName}`} />
+                </figure>
                 <h2 className="card-title">{product.ProductName}</h2>
                 <p>{product.ProductDescription}</p>
                 <div className="card-actions justify-end">
                   ${product.ProductPrice}
-                  <button className="btn btn-primary" onClick={addToCart} value={product.ProductId}>Add to Cart</button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={addToCart}
+                    value={product.ProductId}
+                  >
+                    Add to Cart
+                  </button>
                 </div>
-                
               </div>
             );
           })}
       </div>
-
     </div>
   );
 };
-
-
-
 
 export default Home;
