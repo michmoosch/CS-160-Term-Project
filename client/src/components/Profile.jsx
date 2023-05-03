0c891bf672bd0921801041dc344443f4ad40aa9f
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2023-05-03 14:22:29 -0700

    Profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -8,11 +8,77 @@ const Profile = () => {
   if (!cookie) {
     navigate("/login");
   }
-  const name = cookie.UserFirstName;
+
+  const firstname = cookie.UserFirstName;
+  const lastname = cookie.UserLastName;
+  const email = cookie.UserEmail;
+  const address = cookie.UserAddress;
   const isAdmin = cookie.isAdmin;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // do something with the form data, e.g. send it to a server
+    console.log({ lastName, firstName, email, address });
+  };
+
   return (
     <>
-      <div>Welcome, {name}</div>
+      <div>Welcome, {firstname}</div>
+
+      <div className="my-4">
+        <h1 className="text-center">Profile</h1>
+      </div>
+
+      <form className="flex flex-col items-center ">
+        <div className="form-control my-4">
+          <label className="input-group">
+            <span>Last Name</span>
+            <input
+              type="text"
+              placeholder={lastname}
+              className="input input-bordered"
+              // onChange={(event) => setLastName(event.target.value)}
+            />
+          </label>
+        </div>
+
+        <div className="form-control my-4">
+          <label className="input-group">
+            <span>First Name</span>
+            <input
+              type="text"
+              placeholder={firstname}
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+
+        <div className="form-control my-4">
+          <label className="input-group">
+            <span>Email</span>
+            <input
+              type="text"
+              placeholder={email}
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+
+        <div className="form-control my-4">
+          <label className="input-group">
+            <span>Address</span>
+            <input
+              type="text"
+              placeholder={address}
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+        <div className="my-4">
+          <button className="btn btn-primary mx-4">Edit</button>
+          <button className="btn btn-accent mx-4">Save</button>
+        </div>
+      </form>
     </>
   );
 };
