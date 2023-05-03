6b185f19c35df326962384d44bda75f87d3bafb3
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2023-05-02 18:02:35 -0700

    latest frontend change

--- Frontend/office-supply-depot/src/hooks/useCart.jsx
+++ Frontend/office-supply-depot/src/hooks/useCart.jsx
@@ -3,11 +3,27 @@ import { useUIContext } from "../context/ui";
 function useCart(product) {
   const { cart, setCart } = useUIContext();
 
+  // change addToCart to just add the product to the cart with the quantity of 1
+  // if the product is already in the cart, remove it from the cart
+
   const addToCart = () => {
-    cart.findIndex((c) => c.id === product.id) >= 0
-      ? setCart(cart.filter((c) => c.id !== product.id))
-      : setCart((c) => [...c, product]);
+    const itemIndex = cart.findIndex((c) => c.id === product.id);
+    if (itemIndex >= 0) {
+      const updatedCart = [...cart];
+      updatedCart.splice(itemIndex, 1);
+      setCart(updatedCart);
+    } else {
+      const updatedCart = [...cart];
+      updatedCart.push({ ...product, quantity: 1 });
+      setCart(updatedCart);
+    }
   };
+
+  // const addToCart = () => {
+  //   cart.findIndex((c) => c.id === product.id) >= 0
+  //     ? setCart(cart.filter((c) => c.id !== product.id))
+  //     : setCart((c) => [...c, product]);
+  // };
 
   const addToCartText =
     cart.findIndex((c) => c.id === product.id) >= 0
@@ -18,3 +34,4 @@ function useCart(product) {
 }
 
 export default useCart;
+  
